8fc5ccdd50e6acf4e05264c233f2e794f320821a
Govind9 <dnivogamrahs@gmail.com>
2016-06-29 23:58:24 +0530

    Adding querying mechanism to proceedings. UI: pending. Defense: Pending.

--- Sim_site/Sim_site/urls.py
+++ Sim_site/Sim_site/urls.py
@@ -23,4 +23,6 @@ urlpatterns = [
     url(r'^index/$', views.index, name='index'),
     url(r'^getfile/$', views.getfile, name='getfile'),
     url(r'^index/(?P<name>\w+)/$',views.tables),
+    url(r'^output/$', views.output, name = 'output'),
+    url(r'^table/$', views.table, name = 'table')
 ]
