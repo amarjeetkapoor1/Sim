7966ebeffc7c65cb84ab517ed7eed3614d746d5e
TamandeepSingh <tamandeepsinghkhamba@gmail.com>
2016-10-26 23:27:39 +0530

    adds improvement to urls

--- Sim_site/drawing_freecad/urls.py
+++ Sim_site/drawing_freecad/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     #directs to index veiw
-    url(r'^drawing/$', views.index, name='index'),
+    url(r'^drawing/$', views.indexD, name='indexD'),
     url(r'^drawing/specs/$', views.specs, name='specs'),
     url(r'^drawing/download/$', views.download, name='download'),
     url(r'^drawing/drawing/$', views.drawing, name='drawing')
